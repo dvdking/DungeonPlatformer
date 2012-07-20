0be2adf3e7334499df02c8850a50a4f8dfd11d9a
Sergey <dvdking@i.ua>
2012-07-20 21:23:53 +0400

    AnimSprite,GameObject

---
+++ DungeonPlatformer/DungeonPlatformer/GameObjects/GameObject.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace DungeonPlatformer.GameObjects
+{
+    public abstract class GameObject
+    {
+        private float _x, _y;
+
+        public float X
+        {
+            get { return _x; }
+            set { _x = value; }
+
+        }
+
+
+        public float Y
+        {
+            get { return _y; }
+            set { _y = value; }
+        }
+
+        public Vector2 Position
+        {
+            get {return new Vector2(_x, _y);}
+            set 
+            { 
+                _x = value.X;
+                _y = value.Y;
+            }
+        }
+        private int _width, _height;
+
+        public int Width
+        {
+            get { return _width; }
+            set { _width = value; }
+
+        }
+
+
+        public int Height
+        {
+            get { return _height; }
+            set { _height = value; }
+        }
+
+        public Size Size
+        {
+            get {return new Size(_width, _height);}
+            set
+            {
+                _width = value.Width;
+                _height = value.Height;
+            }
+        }
+
+        public Microsoft.Xna.Framework.Rectangle Bounds
+        {
+            get { return new Microsoft.Xna.Framework.Rectangle((int)_x, (int)_y, _width, _height); }
+            set
+            {
+                _x = value.X;
+                _y = value.Y;
+                _width = value.Width;
+                _height = value.Height;
+            }
+        }
+
+        public abstract void Update(float dt);
+        public abstract void Draw(float dt);
+    }
+}
---
+++ DungeonPlatformer/DungeonPlatformer/Helpers/AnimSprite.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace DungeonPlatformer.Helpers
+{
+    public class AnimSprite
+    {
+        public int Frame;
+        public float TimePerFrame;
+        private int _framesCount;
+
+        public Texture2D Texture { get; private set; }
+
+        public Rectangle SourceRect
+        {
+            get { return new Rectangle(Frame*_frameWidth, 
+                                        Frame*_frameHeight,
+                                        _frameWidth,
+                                        _frameHeight); }
+        }
+
+        private int _frameWidth, _frameHeight;
+        private float _elapsed;
+
+        public AnimSprite(Texture2D texture,int framesCount, int frameWidth, int frameHeight)
+        {
+            Texture = texture;
+            _frameHeight = frameHeight;
+            _frameWidth = frameWidth;
+            _framesCount = framesCount;
+        }
+
+
+        public void Update(float dt)
+        {
+            _elapsed += dt;
+            if(_elapsed > TimePerFrame)
+            {
+                Frame++;
+                if(Frame > _framesCount)
+                    Frame = 0;
+
+                _elapsed -= TimePerFrame;
+            }
+        }
+    }
+
+    public static class AnimExtenstion
+    {
+        
+        public static void Draw(this SpriteBatch s, AnimSprite animSprite, Rectangle rect, Color color)
+        {
+            s.Draw(animSprite.Texture, rect, animSprite.SourceRect, color);
+        }
+    }
+}
